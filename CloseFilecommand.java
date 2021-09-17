fa0d5d1160dac661afe9dbdb06d4e713379620c3
sdadige <90069155+sdadige@users.noreply.github.com>
2021-09-17 09:31:10 +0530

    Create CloseFilecommand.java

---
+++ CloseFilecommand.java
@@ -0,0 +1,16 @@
+package command_pattern;
+
+
+public class CloseFileCommand implements Command {
+
+	private FileSystemReceiver fileSystem;
+	
+	public CloseFileCommand(FileSystemReceiver fs){
+		this.fileSystem=fs;
+	}
+	@Override
+	public void execute() {
+		this.fileSystem.closeFile();
+	}
+
+}
