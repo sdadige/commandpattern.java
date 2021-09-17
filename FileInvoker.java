15a4392b85154e20475b3add835567d222fb156b
sdadige <90069155+sdadige@users.noreply.github.com>
2021-09-17 09:32:32 +0530

    Create FileInvoker.java

---
+++ FileInvoker.java
@@ -0,0 +1,15 @@
+package command_pattern;
+
+
+public class FileInvoker {
+  public Command command;
+	
+	public FileInvoker(Command c){
+		this.command=c;
+	}
+	
+	public void execute(){
+		this.command.execute();
+	}
+}  
+
